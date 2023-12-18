c98e6b224df7b45ded97dfc9c5af63b74ac5e841
Blessing Asakpam <basakpam@st.ug.edu.gh>
2023-12-18 08:53:27 +0000

    https://github.com/asakpam/mini-ia.git

--- server.js
+++ server.js
@@ -1,13 +1,75 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
-//routes
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/', (req, res)=> {
-    res.send('Hello Node API')
-})
+const app = express();
 
-app.listen(3000, ()=> {
-  console.log('Node API app is running on port 3000')
-})
+app.use(bodyParser.json());
+
+const Patient = require('./models/Patient');
+const Encounter = require('./models/Encounter');
+const Vital = require('./models/Vital');
+
+app.post('/patients', async (req, res) => {
+    const newPatient = new Patient(req.body);
+
+    try {
+        await newPatient.save();
+        res.status(201).json(newPatient);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+app.post('/encounters', async (req, res) => {
+    const newEncounter = new Encounter(req.body);
+
+    try {
+        await newEncounter.save();
+        res.status(201).json(newEncounter);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+app.post('/vitals', async (req, res) => {
+    const newVital = new Vital(req.body);
+
+    try {
+        await newVital.save();
+        res.status(201).json(newVital);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+app.get('/patients', async (req, res) => {
+    try {
+        const patients = await Patient.find();
+        res.status(200).json(patients);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/patients/:id', async (req, res) => {
+    try {
+        const patient = await Patient.findById(req.params.id);
+        res.status(200).json(patient);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
